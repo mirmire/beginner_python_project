6fb4dfec2148a160a9c55cc41cdec8895e73f0ba
Prakash Acharya <33595598+mirmire@users.noreply.github.com>
2018-03-18 23:07:54 +0200

    added comments with condition information

--- whats_my_number.py
+++ whats_my_number.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# This problem has seven conditions to be met which is written below
 
 
 def summation(num):
@@ -33,18 +34,30 @@ def is_even(num):
 
 
 def last_digit(num):
+    # look at this for example :)
     if str(num)[-1] == str(len(str(num))):
         return True
     else:
         return False
 
 
+# The number is between 1 and 1000
 for num in range(1, 1001):
+    # The number has 2 or more digits
     if len(str(num)) > 1:
+        # The number is a prime
         if is_prime(num):
+            # The number does not contain 1 or 7 in it
             if '1' not in str(num) and '7' not in str(num):
+                # The sum of all the digits is less or equal to 10
                 if summation(num) <= 10:
+                    # The fitst two digits add up to be odd
                     if is_odd(num):
+                        # The second to last digit is even
                         if is_even(num):
+                            # The last digit is equal to how many digits are
+                            # in the number
                             if last_digit(num):
+                                # It produces two numbers but
+                                # you will understand :)
                                 print(num)
